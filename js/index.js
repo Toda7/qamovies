fe5b6e01372445feefe2bf41ab405d5504f83afd
MoMilosh <milosh.pejic@gmail.com>
2018-10-01 14:48:35 +0200

    povezao sam index.html sa index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 var password;
 var pass1="pingpong"
 password=prompt('Enter Password to View Page','');
-if (password==pass1)
-alert('Correct password, click OK to enter.');
+    if (password==pass1)
+    alert('Correct password, click OK to enter.');
 else
     {
     window.location="http://www.theuselessweb.com/" ;
-}
+    }
