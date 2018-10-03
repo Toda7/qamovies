a35ea5c9ecd5d193082ffd07e1738877b6de1c11
MoMilosh <milosh.pejic@gmail.com>
2018-10-03 13:17:35 +0200

    javascript izmene

--- js/milos.js
+++ js/milos.js
@@ -1,3 +1,5 @@
+var c = "Congratulations, your application has been sent! Enjoy"
 function funkcija1(){
-    alert("Congratulations!")
+    alert("Please confirm your application");
+    alert(c);
 }
