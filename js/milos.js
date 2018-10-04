7b045cab4a4ad4e2b3ea1ef1850dfd99a01487fc
MoMilosh <milosh.pejic@gmail.com>
2018-10-04 16:23:52 +0200

    editovana skripta za formu

--- js/milos.js
+++ js/milos.js
@@ -2,13 +2,16 @@ var p = "Please confirm your application"
 var c = "Congratulations, your application has been sent! Enjoy"
 var t = "Please try again"
 var t = "Do you want to reset form?"
+var m = "Thank you, enjoy our site."
+
 
 
 
 function funkcija1() {
     alert(p);
-if (p) {
     alert(c);
+if (c) {
+    document.getElementById("jsbutton").innerHTML = m;
 } else {
     alert(t); 
 }
