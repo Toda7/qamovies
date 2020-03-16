e4fb2d8b4c1cd6b2929ca6cafa9553b062547933
MoMilosh <milosh.pejic@gmail.com>
2020-03-16 14:51:39 +0100

    dodao sam cekere u create discussion test

--- tests/private/community/createDiscussion/createDiscussion-po.js
+++ tests/private/community/createDiscussion/createDiscussion-po.js
@@ -10,10 +10,28 @@ function landOnHome() {
     browser.sleep(1000);
  }
 
+function checkSelectAudienceTitleOnPage(){
+   // Provera Select Audience title na strani 
+   var xxx = element(by.tagName('h4'));
+   expect(xxx.getText()).toEqual('Select Audience');
+}
+
+function checkTextOnPage(){
+   // Provera texta na strani 
+   var xxx = element(by.tagName('p'));
+   expect(xxx.getText()).toEqual('Your account allows you to post content to different groups. Please select an audience.');
+}
+
  function selectGroup() {
    //  Odaberi grupu
    element(by.cssContainingText('span.title', 'Professionals')).click();
    browser.sleep(1000); 
+}
+
+function checkSelectedGroup(){
+   // Provera da li smo izabrali dobru grupu
+   var xxx = element(by.className('collection-item avatar'));
+   expect(xxx.getText()).toEqual('FI Professionals\nPeople are added to the bankers group if there are an employee of an FI.');
 }
 
 function discussionTitle() {
@@ -53,14 +71,81 @@ function checkSuccess(){
    browser.sleep(2000);
 }  
 
+function clickOnViewDiscussion() {
+   //  Klik na View Discussion dugme
+   element(by.className('btn btn-flat')).click(); 
+   browser.sleep(1000);
+}
+
+// cekeri za discussion card 
+function checkTitleOfDiscussion() {
+   // Provera title of discussion 
+   var xxx = element(by.tagName('h1'));
+   expect(xxx.getText()).toEqual('ATQA Title');
+}
+
+function checkTopicsOfDiscussion() {
+   // Provera topics of discussion 
+   var xxx = element(by.className('tags'));
+   expect(xxx.getText()).toEqual('Topics: Credit Union');
+}
+
+function checkUserName(){
+   // Provera da li se ucitao user name
+   var EC = protractor.ExpectedConditions;
+   browser.wait(EC.textToBePresentInElement($('.community .signature .constrain .title .person_name'), 'Chicago Alberto Diaz FI'), 5000);
+}
+
+function checkFunctionLevel(){
+   // Provera da li se ucitao function level
+   var EC = protractor.ExpectedConditions;
+   browser.wait(EC.textToBePresentInElement($('.community .signature .constrain .info .org_function'), 'Employee - Technology'), 5000);
+}
+
+function checkOrgSize(){
+   // Provera da li se ucitao org size
+   var EC = protractor.ExpectedConditions;
+   browser.wait(EC.textToBePresentInElement($('.community .signature .constrain .info .org_size'), '($500M | IL)'), 5000);
+}
+
+function checkOrgLink(){
+   // Provera da li se ucitao org link
+   var EC = protractor.ExpectedConditions;
+   browser.wait(EC.textToBePresentInElement($('.community .signature .constrain .info .org_link'), 'Bank of Chicago'), 5000);
+}
+
+function checkDescriptionOfDiscussion(){
+   // Provera da li se ucitao description of discussion
+   var EC = protractor.ExpectedConditions;
+   browser.wait(EC.textToBePresentInElement($('.community .html_content'), 'ATQA Description description testing credit'), 5000);
+}
+
+function checkDocumentInDiscussionCard(){
+   // Provera dokumenta u discussion card
+   var xxx = element(by.className('attachment txt'));
+   expect(xxx.getText()).toEqual('TXT\nTestTXT.txt');
+}
+
  module.exports = {
     landOnHome,
     clickOnStartDiscussion,
+    checkSelectAudienceTitleOnPage,
+    checkTextOnPage,
     selectGroup,
+    checkSelectedGroup,
     discussionTitle,
     discussionDesc,
     attachDoc,
     selectTopic,
     postDiscussion,
     checkSuccess,
+    clickOnViewDiscussion,
+    checkTitleOfDiscussion,
+    checkTopicsOfDiscussion,
+    checkUserName,
+    checkFunctionLevel,
+    checkOrgSize,
+    checkOrgLink,
+    checkDescriptionOfDiscussion,
+    checkDocumentInDiscussionCard,
  }
