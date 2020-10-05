160ef6edb3642ad01bf52d8dcf2e1f121a550f9a
MoMilosh <milosh.pejic@guidefitter.com>
2020-10-05 20:47:36 +0200

    chekeri za header 1

--- tests/public/staticHome/staticHome-po.js
+++ tests/public/staticHome/staticHome-po.js
@@ -81,6 +81,264 @@ function checkBottomSection(){
     expect(xxx.getText()).toEqual('Join the largest online network of verified banking and credit union professionals.\nGet your questions answered, exchange ideas, and build your industry reputation.\nJOIN FOR FREE');
 }
 
+// Chekeri za Header linkove 
+function checkCBlogoInHeader() {
+    // Provera da li se ucitao Cbanc Logo u headeru
+    var xxx = element.all(by.tagName('img')).get(0);
+    expect(xxx.getAttribute('src')).toEqual('https://static.cbancnetwork.com/Public/higgins/0.0.1/img/cbanc-logo-transparent.svg');
+ }
+
+function clickOnLogo() {
+    //  Klik na Logo u headeru
+     element(by.id("logo")).click();
+}
+
+ function checkHomeURL() {
+    // Provera home URL-a nakon klika na logo
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com'), 7000);
+}
+
+function checkLinksInHeader(){
+    // Provera da li su se ucitali Community, Education, More, Vendors linkovi i JOIN i SIGN IN buttni u headeru
+    var xxx = element(by.className('header_main'));
+    expect(xxx.getText()).toEqual('Community\nEducation\nMore\nVendors\nJOIN\nSIGN IN');
+}
+
+function clickOnCommunity() {
+    //  Klik na Community u headeru
+     element(by.className("menu_item link_community has_submenu")).click();
+ }
+
+function HoverToOpenCommunityDrop() {
+    // Hover to open Community drop menu
+    browser.actions().mouseMove(element(by.css('.menu_item.link_community.has_submenu'))).perform();
+    expect(element(by.css('.menu_item.link_community.has_submenu')).isDisplayed()).toBeTruthy();
+}
+
+function checkSubmenuSectionFromCommunityDrop(){
+    // Provera submenu sekcije u Comunity drop downu
+    var xxx = element(by.className('submenu'));
+    expect(xxx.getText()).toEqual('DOCUMENT LIBRARY\nA searchable, sortable archive of the documents uploaded to CBANC.\nGET ANSWERS\nThe latest discussions in the CBANC community.\nTOPICS\nThe most popular topics on CBANC. Subscribe to the ones that interest you.');
+}
+
+function clickOnDocumentLink() {
+    //  Klik na Document link iz Community drop down menu
+     element(by.className("documents")).click();
+}
+
+ function checkDocumentsURL() {
+    // Provera document URL-a nakon klika na Documents link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/documents'), 7000);
+}
+
+function clickOnTopicsLink() {
+    //  Klik na topics link iz Community drop down menu
+     element(by.className("topics")).click();
+}
+
+ function checkTopicssURL() {
+    // Provera Topics URL-a nakon klika na topicss link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/topics'), 7000);
+}
+
+function clickOnQuestionsLink() {
+    //  Klik na questions link iz Community drop down menu
+     element(by.className("questions")).click();
+}
+
+ function checkQuestionsURL() {
+    // Provera questions URL-a nakon klika na questionss link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/questions'), 7000);
+}
+
+function clickOnEducationLink() {
+    //  Klik na Education u headeru
+     element(by.className("menu_item link_education has_submenu")).click();
+ }
+
+ function checkEducationURL() {
+    // Provera Education URL-a nakon klika na education link u headeru
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/cbanc-education'), 7000);
+}
+
+function HoverToOpenEducationDrop() {
+    // Hover to open Education drop menu
+    browser.actions().mouseMove(element(by.css('.menu_item.link_education.has_submenu'))).perform();
+    expect(element(by.css('.menu_item.link_education.has_submenu')).isDisplayed()).toBeTruthy();
+}
+
+function checkSubmenuSectionFromEducationyDrop(){
+    // Provera submenu sekcije u Education drop downu
+    var xxx = element.all(by.className('submenu')).get(1);
+    expect(xxx.getText()).toEqual('TRAINING - NEW!\nEnroll in CBANC and partners online certification training programs.\nUPCOMING WEBINARS\nThe best content delivered live, offered with CPE credits, from experts in the financial industry.\nBUNDLES\nIncrease your professional value with these curated, on demand bundled webinars.\nON DEMAND\nDiscover a past webinar or replay webinar - including content from the ABA - on your schedule.\nALL-ACCESS PLUS\nThe most cost effective training for your entire financial institution.\nORDERS\nSee your order history, print CPE certificates and receipts, etc.');
+}
+
+function clickOnTrainingLink() {
+    //  Klik na Training link iz Education drop down menu
+     element(by.className("lms_training")).click();
+}
+
+function checkTitleInSignInForm(){
+    // Provera Titla na Sign in strani, ovo radim da bih imao duzi wait jer dugo vremena treba da se ucita training strana
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.textToBePresentInElement($('.auth0-lock-name'), 'CBANC Training'), 20000);
+}
+
+function clickOnUpcomingLink() {
+    //  Klik na Upcoming Webinars link iz Education drop down menu
+     element(by.className("events")).click();
+}
+
+function checkUpcomingURL() {
+    // Provera Upcoming URL-a nakon klika na Upcoming Webinars link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/education/webinars/calendar'), 7000);
+}
+
+function clickOnBundlesLink() {
+    //  Klik na Bundles link iz Education drop down menu
+     element(by.className("bundles")).click();
+}
+
+function checkBundlesURL() {
+    // Provera Bundles URL-a nakon klika na Bundles link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/education/bundles'), 7000);
+}
+
+function clickOnOnDemandLink() {
+    //  Klik na OnDemand link iz Education drop down menu
+     element(by.className("ondemand")).click();
+}
+
+function checkOnDemandURL() {
+    // Provera OnDemand URL-a nakon klika na OnDemand link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/education/webinars/on_demand'), 7000);
+}
+
+function clickOnAllAccessLink() {
+    //  Klik na All Access link iz Education drop down menu
+     element(by.className("all_access_plus")).click();
+}
+
+function checkAllAccessURL() {
+    // Provera All Access URL-a nakon klika na All Access link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/cbanc-education'), 7000);
+}
+
+function clickOnOrdersLink() {
+    //  Klik na Orders link iz Education drop down menu
+     element(by.className("orders")).click();
+}
+
+function checkOrdersURL() {
+    // Provera Orders URL-a nakon klika na Orders link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/education/orders/my_orders'), 7000);
+}
+
+function clickOnMoreLink() {
+    //  Klik na More link u headeru
+     element(by.className("menu_item link_apps has_submenu")).click();
+ }
+
+ function checkMoreURL() {
+    // Provera More URL-a nakon klika na More link u headeru
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/vendor-management'), 7000);
+}
+
+function HoverToOpenMoreDrop() {
+    // Hover to open More drop menu
+    browser.actions().mouseMove(element(by.css('.menu_item.link_apps.has_submenu'))).perform();
+    expect(element(by.css('.menu_item.link_apps.has_submenu')).isDisplayed()).toBeTruthy();
+}
+
+function checkSubmenuSectionFromMoreyDrop(){
+    // Provera submenu sekcije u More drop downu
+    var xxx = element.all(by.className('submenu')).get(2);
+    expect(xxx.getText()).toEqual('VENDOR MANAGEMENT\nCompliance management application for banks and credit unions.\nRISK ASSESSMENT\nFree, secure risk analysis tool for banks and credit unions.\nPEER ANALYSIS\nCustom peer group analysis for free.\nMORTGAGE SETTLEMENT SERVICES\nIntegrated mortgage settlement services software and provider marketplace.');
+}
+
+function clickOnVendorManagementLink() {
+    //  Klik na vendor management link iz Education drop down menu
+     element(by.className("vendor_management")).click();
+}
+
+function checkVendorManagementURL() {
+    // Provera vendor management URL-a nakon klika na vendor management link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/vendor-management'), 7000);
+}
+
+function clickOnRiskAssessmentLink() {
+    //  Klik na Risk Assessment link iz Education drop down menu
+     element(by.className("risk_assessment")).click();
+}
+
+function checkRiskAssessmentURL() {
+    // Provera Risk Assessment URL-a nakon klika na Risk Assessment link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/risk_assessment'), 7000);
+}
+
+function clickOnPeerAnalysisLink() {
+    //  Klik na Peer Analysis link iz Education drop down menu
+     element(by.className("peer_group_analysis")).click();
+}
+
+function checkPeerAnalysisURL() {
+    // Provera Peer Analysis URL-a nakon klika na Peer Analysis link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/peer-group-analysis'), 7000);
+}
+
+function clickOnLendwellLink() {
+    //  Klik na Lendwell link iz Education drop down menu
+     element(by.className("lendwell")).click();
+}
+
+function checkLendwellURL() {
+    // Provera Lendwell URL-a nakon klika na Lendwell link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/lendwell'), 7000);
+}
+
+function clickOnVendorLink() {
+    //  Klik na Vendor link u headeru
+     element(by.className("menu_item link_viewfinder")).click();
+}
+
+function checkVendorURL() {
+    // Provera Vendor URL-a nakon klika na Vendor link 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/view-finder'), 7000);
+}
+
+function clickOnSignInButton() {
+    //  Klik na Sign In button u headeru
+     element(by.className("sign_in")).click();
+}
+
+function clickOnJoinButton() {
+    //  Klik na Join button u headeru
+     element(by.className("join")).click();
+}
+
+function checkJoinURL() {
+    // Provera Join URL-a nakon klika na Join button 
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://qa.cbancnetwork.com/membership/join/'), 7000);
+}
+
+
 module.exports = {
     checkPageTitle,
     checkButton,
@@ -95,4 +353,51 @@ module.exports = {
     checkVendorImage,
     checkVendorSection,
     checkBottomSection,
+    checkCBlogoInHeader,
+    clickOnLogo,
+    checkHomeURL,
+    checkLinksInHeader,
+    clickOnCommunity,
+    HoverToOpenCommunityDrop,
+    checkSubmenuSectionFromCommunityDrop,
+    clickOnDocumentLink,
+    checkDocumentsURL,
+    clickOnTopicsLink,
+    checkTopicssURL,
+    clickOnQuestionsLink,
+    checkQuestionsURL,
+    clickOnEducationLink,
+    checkEducationURL,
+    HoverToOpenEducationDrop,
+    checkSubmenuSectionFromEducationyDrop,
+    clickOnTrainingLink,
+    checkTitleInSignInForm,
+    clickOnUpcomingLink,
+    checkUpcomingURL,
+    clickOnBundlesLink,
+    checkBundlesURL,
+    clickOnOnDemandLink,
+    checkOnDemandURL,
+    clickOnAllAccessLink,
+    checkAllAccessURL,
+    clickOnOrdersLink,
+    checkOrdersURL,
+    clickOnMoreLink,
+    checkMoreURL,
+    HoverToOpenMoreDrop,
+    checkSubmenuSectionFromMoreyDrop,
+    clickOnVendorManagementLink,
+    checkVendorManagementURL,
+    clickOnRiskAssessmentLink,
+    checkRiskAssessmentURL,
+    clickOnPeerAnalysisLink,
+    checkPeerAnalysisURL,
+    clickOnLendwellLink,
+    checkLendwellURL,
+    clickOnVendorLink,
+    checkVendorURL,
+    clickOnSignInButton,
+    clickOnJoinButton,
+    checkJoinURL,
+
 }
--- tests/public/staticHome/staticHome-spec.js
+++ tests/public/staticHome/staticHome-spec.js
@@ -1,6 +1,7 @@
 var register = require('../registracija/registracija-po');
 var staticHome = require('./staticHome-po');
 var staticLendwell = require('../staticLendwell/staticLendwell-po');
+var login = require('../login/login-po');
 
 
 describe('Home page', function(){
@@ -70,6 +71,340 @@ describe('Home page', function(){
         it('Provera da li se ucitao intercom', function(){
             staticLendwell.checkIntercom();
         });
+
+        // Chekeri za Header linkove
+        it('Provera da li se ucitao Cbanc Logo u headeru', function(){
+            staticHome.checkCBlogoInHeader();
+        });
+
+        it('Klik na Logo u headeru', function(){
+            staticHome.clickOnLogo();
+            browser.sleep(2000);
+        });
+
+        it('Provera home URL-a nakon klika na logo', function(){
+            staticHome.checkHomeURL();
+        });
+
+        it('Provera da li su se ucitali Community, Education, More, Vendors linkovi i JOIN i SIGN IN buttni u headeru', function(){
+            staticHome.checkLinksInHeader();
+            browser.sleep(1000);
+        });
+
+        it('Klik na Community u headeru', function(){
+            staticHome.clickOnCommunity();
+            browser.sleep(3000);
+        });
+
+        it('Unosenje emaila u Email polje da bi potvrdili da se ucitala sign in forma', function(){
+            login.enterEmail();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Community drop menu', function(){
+            staticHome.HoverToOpenCommunityDrop();
+            browser.sleep(2000);
+        });
+
+        it('Provera submenu sekcije u Comunity drop downu', function(){
+            staticHome.checkSubmenuSectionFromCommunityDrop();
+        });
+
+        it('Klik na Document link iz Community drop down menu', function(){
+            staticHome.clickOnDocumentLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera document URL-a nakon klika na Documents link ', function(){
+            staticHome.checkDocumentsURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Community drop menu', function(){
+            staticHome.HoverToOpenCommunityDrop();
+            browser.sleep(2000);
+        });
+
+
+        it('Klik na Topics link iz Community drop down menu', function(){
+            staticHome.clickOnTopicsLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Topics URL-a nakon klika na topics link ', function(){
+            staticHome.checkTopicssURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Community drop menu', function(){
+            staticHome.HoverToOpenCommunityDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Questions link iz Community drop down menu', function(){
+            staticHome.clickOnQuestionsLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Questions URL-a nakon klika na questions link ', function(){
+            staticHome.checkQuestionsURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Education link u headeru', function(){
+            staticHome.clickOnEducationLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Education URL-a nakon klika na Education link ', function(){
+            staticHome.checkEducationURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Provera submenu sekcije u Education drop downu', function(){
+            staticHome.checkSubmenuSectionFromEducationyDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Training link iz Education drop down menu', function(){
+            staticHome.clickOnTrainingLink();
+            browser.sleep(10000);
+        });
+
+        it('Provera Titla na Sign in strani', function(){
+            staticHome.checkTitleInSignInForm();
+        });
+
+        it('Unosenje email u Email polje da bi potvrdili da smo dobili Sign in formu nakon klika na Training link', function(){
+            login.enterEmail();
+            browser.sleep(1000);
+        });
+
+        it('Dolazak na home stranu jer ne radi dobro BACK sa Training strane', function(){
+            register.landOnHomePage();
+            browser.sleep(4000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Upcoming Webinars link iz Education drop down menu', function(){
+            staticHome.clickOnUpcomingLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Upcoming URL-a nakon klika na Upcoming Webinars link ', function(){
+            staticHome.checkUpcomingURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Bundles link iz Education drop down menu', function(){
+            staticHome.clickOnBundlesLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Bundles URL-a nakon klika na Bundles link', function(){
+            staticHome.checkBundlesURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na OnDemand link iz Education drop down menu', function(){
+            staticHome.clickOnOnDemandLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera OnDemand URL-a nakon klika na On Demand link', function(){
+            staticHome.checkOnDemandURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na All Access link iz Education drop down menu', function(){
+            staticHome.clickOnAllAccessLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera All Access URL-a nakon klika na All Access link', function(){
+            staticHome.checkAllAccessURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open Education drop menu', function(){
+            staticHome.HoverToOpenEducationDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Orders link iz Education drop down menu', function(){
+            staticHome.clickOnOrdersLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Orders URL-a nakon klika na Orders link ', function(){
+            staticHome.checkOrdersURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Klik na More link u headeru', function(){
+            staticHome.clickOnMoreLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera More URL-a nakon klika na More link u headeru', function(){
+            staticHome.checkMoreURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(3000);
+        });
+
+        it('Hover to open More drop menu', function(){
+            staticHome.HoverToOpenMoreDrop();
+            browser.sleep(2000);
+        });
+
+       it('Provera submenu sekcije u More drop downu', function(){
+            staticHome.checkSubmenuSectionFromMoreyDrop();
+        });
+
+        it('Klik na vendor management link iz Education drop down menu', function(){
+            staticHome.clickOnVendorManagementLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera vendor management URL-a nakon klika na vendor management link', function(){
+            staticHome.checkVendorManagementURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open More drop menu', function(){
+            staticHome.HoverToOpenMoreDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Risk Assessment link iz Education drop down menu', function(){
+            staticHome.clickOnRiskAssessmentLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Risk Assessment URL-a nakon klika na Risk Assessment link', function(){
+            staticHome.checkRiskAssessmentURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open More drop menu', function(){
+            staticHome.HoverToOpenMoreDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Peer Analysis link iz Education drop down menu', function(){
+            staticHome.clickOnPeerAnalysisLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Peer Analysis URL-a nakon klika na Peer Analysis link', function(){
+            staticHome.checkPeerAnalysisURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Hover to open More drop menu', function(){
+            staticHome.HoverToOpenMoreDrop();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Lendwell link iz Education drop down menu', function(){
+            staticHome.clickOnLendwellLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Lendwell URL-a nakon klika na Lendwell link', function(){
+            staticHome.checkLendwellURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Vendor link u headeru', function(){
+            staticHome.clickOnVendorLink();
+            browser.sleep(2000);
+        });
+
+        it('Provera Vendor URL-a nakon klika na Vendor link', function(){
+            staticHome.checkVendorURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Sign In button u headeru', function(){
+            staticHome.clickOnSignInButton();
+            browser.sleep(2000);
+        });
+
+        it('Unosenje email u Email polje da bi potvrdili da smo dobili Sign in formu nakon klika na Sign in button', function(){
+            login.enterEmail();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
+        it('Klik na Join button u headeru', function(){
+            staticHome.clickOnJoinButton();
+            browser.sleep(2000);
+        });
+
+        it('Provera Join URL-a nakon klika na Join button', function(){
+            staticHome.checkJoinURL();
+            browser.sleep(1000);
+            browser.navigate().back();
+            browser.sleep(2000);
+        });
+
 
     afterAll(function(){
         browser.waitForAngularEnabled(true); 
