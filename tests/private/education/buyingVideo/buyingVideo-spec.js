f64e075c45e95bb407fcc0a3741dcf9003fee796
MoMilosh <milosh.pejic@guidefitter.com>
2020-12-01 13:47:14 +0100

    fix for webinars

--- tests/private/education/buyingVideo/buyingVideo-spec.js
+++ tests/private/education/buyingVideo/buyingVideo-spec.js
@@ -5,119 +5,147 @@ describe('Buying video ', function(){
 
 beforeAll(function(){
     browser.waitForAngularEnabled(false);
+    browser.sleep(1000);
 });
 
 it('Dolazak na webinras stranu', function(){
     buyingVideo.landOnVideoPage();
+    browser.sleep(4000);
 });
 
 it('Unosenje texta u search polje', function(){
     buyingVideo.enterTextInSearchField();
+    browser.sleep(1000);
 });
 
 it('Klik on search option', function(){
     buyingHostedWebinar.clickOnSearchOption();
+    browser.sleep(3000);
 });
 
 it('Klik on first video on the list of search', function(){
     buyingHostedWebinar.clickOnFirstWebinar();
+    browser.sleep(5000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
+    browser.sleep(1000);
 });
 
 it('Provera da li se pojavila prva error poruka', function(){
     buyingHostedWebinar.checkFirstErrorMessage();
+    browser.sleep(1000);
 });
 
 it('Enter numbers in credit card field', function(){
     buyingVideo.enterNumbersInCreditCardField();
+    browser.sleep(1000);
 });
 
 it('Enter name of credit card field', function(){
     buyingVideo.enterNameOfCreditCard();
+    browser.sleep(1000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
+    browser.sleep(1000);
 });
 
 it('Provera da li se pojavila druga error poruka', function(){
     buyingHostedWebinar.checkSecondErrorMessage();
+    browser.sleep(1000);
 });
 
 it('Enter numbers in month field', function(){
     buyingHostedWebinar.enterNumbersInMonthField();
+    browser.sleep(1000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
+    browser.sleep(1000);
 });
 
 it('Provera da li se pojavila treca error poruka', function(){
     buyingHostedWebinar.checkThirdErrorMessage();
+    browser.sleep(1000);
 });
 
 it('Enter numbers in year field', function(){
     buyingHostedWebinar.enterNumbersInYearField();
+    browser.sleep(1000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
+    browser.sleep(1000);
 });
 
 it('Provera da li se pojavila cetvrta error poruka', function(){
     buyingHostedWebinar.checkFourthErrorMessage();
+    browser.sleep(1000);
 });
 
 it('Enter numbers in CVC field', function(){
     buyingHostedWebinar.enterNumbersInCVCField();
+    browser.sleep(1000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
+    browser.sleep(1000);
 });
 
 it('Provera da li se pojavila error poruka za zip code', function(){
     buyingHostedWebinar.checkErrorMessageForZipCode();
+    browser.sleep(1000);
 });
 
 it('Enter numbers in zip code field', function(){
     buyingVideo.enterNumbersInZipField();
+    browser.sleep(1000);
 });
 
 it('Klik on agree and pay button', function(){
     buyingVideo.clickOnAgreePayButton();
-    browser.sleep(2000);
+    browser.sleep(4000);
 });
 
 it('Provera da li se pojavila "This video has been purchased. message', function(){
     buyingVideo.checkPurchaseMessage();
+    browser.sleep(1000);
 });
 
 it('Hover To Open User Drop', function(){
     buyingHostedWebinar.HoverToOpenUserDrop();
+    browser.sleep(1000);
 });
 
 it('Klik on SingOut', function(){
     buyingHostedWebinar.clickOnSignOutLink();
+    browser.sleep(1000);
 });
 
 it('Klik on SinIn', function(){
     buyingHostedWebinar.clickOnSignIn();
+    browser.sleep(1000);
 });
 
 it('Enter email', function(){
     buyingHostedWebinar.enterEmail();
+    browser.sleep(1000);
 });
 
 it('Enter password', function(){
     buyingHostedWebinar.enterPass();
+    browser.sleep(1000);
 });
 
 it('Klik na Submit', function(){
     buyingHostedWebinar.submit();
+    browser.sleep(1000);
 });
 
 afterAll(function(){
--- tests/private/education/createVideo/createVideo-spec.js
+++ tests/private/education/createVideo/createVideo-spec.js
@@ -6,95 +6,117 @@ describe('Create video', function(){
 
 beforeAll(function(){
     browser.waitForAngularEnabled(false);
+    browser.sleep(1000);
 });
 
 it('Dolazak na admin stranu', function(){
     createHostedWebinar.landOnEducationAdminPage();
+    browser.sleep(4000);
 });
 
 it('Klik on video link', function(){
     createVideo.clickOnVideoLink();
+    browser.sleep(1000);
 });
 
 it('Klik on create video button', function(){
     createVideo.clickOnCreateVideoButton();
+    browser.sleep(1000);
 });
  
 it('Provera da li se ucitao title on page', function(){
     createVideo.checkTitleOnPage();
+    browser.sleep(1000);
 });
 
 it('Unosenje texta u title field', function(){
     createVideo.enterTextInTitleField();
+    browser.sleep(1000);
 });
 
 it('Unosenje texta u synopsis field', function(){
     createHostedWebinar.enterTextInSynopsisField();
+    browser.sleep(1000);
 });
 
 it('Unosenje texta u description field', function(){
     createHostedWebinar.enterTextInDescriptionField();
+    browser.sleep(1000);
 });
 
 it('Unosenje linka u here field', function(){
     createHostedWebinar.enterTextInHereField();
+    browser.sleep(1000);
 });
 
 it('Unosenje linka u tile field', function(){
     createHostedWebinar.enterTextInTileField();
+    browser.sleep(1000);
 });
 
 it('Unosenje text u price field', function(){
     createHostedWebinar.enterTextInPriceField();
+    browser.sleep(1000);
 });
 
 it('Setovanje date', function(){
     createVideo.enterDateField();
+    browser.sleep(1000);
 });
 
 it('Setovanje date', function(){
     createVideo.enterDateField2();
+    browser.sleep(1000);
 });
 
 it('Unosenje video urla u video field', function(){
     createVideo.enterVideoUrlInVideoField();
+    browser.sleep(1000);
 });
 
 it('Unosenje text u Purchase Details field', function(){
     createHostedWebinar.enterTextInPurchaseDetailsField();
+    browser.sleep(1000);
 });
 
 it('Klik on Save video button', function(){
     createVideo.clickOnVideoButton();
-    browser.sleep(3000);
+    browser.sleep(4000);
 });
 
 it('Upload file', function(){
     createHostedWebinar.addDoc();
+    browser.sleep(1000);
 });
 
 it('Select paid option', function(){
     createHostedWebinar.selectPaidForVisibility();
+    browser.sleep(1000);
 });
 
 it('Klik on Upload & Attach button', function(){
     createHostedWebinar.clickOnUploadAttachButton();
+    browser.sleep(5000);
 });
 
 it('Unosenje texta u presenter field', function(){
     createHostedWebinar.enterTextInPresentersField();
+    browser.sleep(1000);
 });
 
 it('Klik on Find & Attach button', function(){
     createHostedWebinar.clickOnFindttachButton();
+    browser.sleep(1000);
 });
 
 it('Select topic tag', function(){
     createHostedWebinar.selectTopicTags();
+    browser.sleep(1000);
 });
 
 it('Klik to Select Topic', function(){
     createHostedWebinar.clickOnTopicToSelect();
+    browser.sleep(1000);
 });
 
 it('Klik on Save Topics button', function(){
